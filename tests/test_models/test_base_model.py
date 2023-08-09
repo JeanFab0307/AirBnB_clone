945bc3b81206e2697fc15d74ecbe18736d5cf00e
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-08-09 08:12:32 -0700

    Add container of empty test suites

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Unittest of the class BaseModel"""
+import unittest
+from unittest import mock
+
+class TestBaseModel(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_normal_input(self):
+        pass
+
+    def test_str(self):
+        pass
+
+    def test_to_dict(self):
+        pass
+
+    def test_save(self):
+        pass
+
+    def test_variable_constructor(self):
+        pass
